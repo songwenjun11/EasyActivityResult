af4278400b086a1152f76482c7e245116a63b5a4
songwenjun <2492824114@qq.com>
2021-10-12 21:00:40 +0800

    first commit

---
+++ src/main/java/com/song/easyactivityresult/activity/ExcessiveActivity.kt
@@ -0,0 +1,16 @@
+package com.song.easyactivityresult.activity
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by SongWenjun
+ * 2021/10/12
+ *       ∩ _ ∩
+ *      (??ω??)っ一? ?? ???
+ *        っ 丿         ? ????
+ *     乚― J               ???
+ * This class is ...
+ */
+class ExcessiveActivity : AppCompatActivity() {
+    
+}
